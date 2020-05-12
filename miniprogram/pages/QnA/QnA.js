c07ff50b0a1391c2dfe7b6980e2660add0d1db90
Jiexin Li <ljxxx1206@gmail.com>
2020-05-13 00:48:54 +0930

    Q&A page - hasn't finished

--- miniprogram/pages/QnA/QnA.js
+++ miniprogram/pages/QnA/QnA.js
@@ -1,23 +1,13 @@
-
+var app = getApp();
+var db = app.globalData.db;
+const qna_list = db.collection("qAndA");
 Page({
   data: {
     activeName: [],
-    qalist: [
-      {
-        title: "请问悉尼大学雅思多少要求？",
-        answer: "留学课代表: balabalablablablablbalablbalbalbalbalbalbalbalbalbalba",
-        icon: "../../../../images/testimg.jpeg"
-      },
-      {
-
-      },
-      {
-
-      },
-      {
-
-      }
-    ]
+    questions:[],
+    searchValue: '',
+    questionsValue: '',
+    userAvatar: ''
   },
   onChange(event) {
     this.setData({
@@ -28,6 +18,93 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    qna_list.get({
+      success: res => {
+        var datas = res.data;
+        this.setData({
+          questions: datas
+        });
+      }
+    })
+  },
 
+  /**
+   * 点击搜索的时候查找
+   */
+  searchClick: function () {
+    var that = this;
+    wx.showLoading({
+      title: '搜索中...',
+    })
+    qna_list.where(db.command.or([
+      {
+        //使用正则查询，实现对搜索的模糊查询
+        title: db.RegExp({
+          regexp: '.*' + that.data.searchValue + '.*',
+          //从搜索栏中获取的value作为规则进行匹配。
+          options: 'i',
+          //大小写不区分
+        })
+      }
+    ])).get({
+      success: res => {
+        wx.hideLoading();
+        var datas = res.data;
+        that.setData({
+          questions: datas
+        })
+        console.log(that.data.questions)
+      }
+    })
+  },
+
+  /**
+  * 每次输入时改变value的值
+  */
+  searchChange: function (e) {
+    this.setData({
+      searchValue: e.detail // e.detail 得到输入的值
+    })
+    if (e.detail == '') {
+      qna_list.get({
+        success: res => {
+          var datas = res.data;
+          this.setData({
+            questions: datas
+          });
+        }
+      })
+    }
+  },
+
+  sendingQuestion: function (e) {
+    wx.showLoading({
+      title: '发布中...',
+    })
+    var that = this;
+    console.log(e.detail.userInfo.avatarUrl)
+    this.setData({
+      userAvatar: e.detail.userInfo.avatarUrl
+    })
+    qna_list.add({
+      data: {
+        title: "请问南澳大学的成绩要求多少?",
+        responser: "",
+        answer: "",
+        icon: that.data.userAvatar
+      },
+      success: res => {
+        qna_list.get({
+          success: res => {
+            wx.hideLoading();
+            var datas = res.data;
+            this.setData({
+              questions: datas
+            });
+          }
+        })
+      }
+    })
   }
+
 })
