fbec7f8e98228f81261878fd4e4f10f58e852896
Jiexin Li <ljxxx1206@gmail.com>
2020-06-16 02:43:02 +0930

    add new page under studying channel page

---
+++ miniprogram/pages/studyingChannel/studyingChannelItem/studyingChannelItem.js
@@ -0,0 +1,35 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isCollapse: true,
+    textHidden: "hidden",
+    hasHeight: "height:500rpx"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  collapse: function() {
+    if(this.data.isCollapse) {
+      this.setData({
+        textHidden: "block",
+        hasHeight: "",
+        isCollapse: false
+      })
+    } else {
+      this.setData({
+        textHidden: "hidden",
+        hasHeight: "height:500rpx",
+        isCollapse: true
+      })
+    }
+  }
+ 
+})
